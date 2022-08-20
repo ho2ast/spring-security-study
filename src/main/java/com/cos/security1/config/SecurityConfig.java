ef919fa39eaa876d7de893c330ae6187a15f72cf
dh.nam <dh.nam@okestro.com>
2022-08-20 22:35:36 +0900

    get user info after oauth login

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,10 +12,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 1. 코드받기(인증) 2. 엑세스토큰(권한)
+// 3. 사용자프로필 정보가져오기 4-1. 그 정보를 토대로 회원가입을 진행
+// 4-2 (이메일, 전화번호, 이름, 아이디) 외에 추가정인 정보(주소, 등급)
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
+// secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    private final PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -21,23 +32,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/user/**").authenticated()
-            .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-            .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
-            .anyRequest().permitAll()
-            .and()
-            .formLogin()
-            .loginPage("/loginForm")
-            .loginProcessingUrl("/login") // "/login"이 호출되면 시큐리티 낚아채서 대신 로그인을 진행합니다.
-            .defaultSuccessUrl("/")
-            .and()
-            .oauth2Login()
-            .loginPage("/loginForm") // 구글 로그인이 완료된 후 후처리가 필요함
-            .and()
-            .build();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // "/login"이 호출되면 시큐리티 낚아채서 대신 로그인을 진행합니다.
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인이 완료된 후 후처리가 필요함. Tip. 코드X (엑세스토큰 + 사용자 프로필 정보)
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
+        return http.build();
     }
 
 }
